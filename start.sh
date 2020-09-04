fcac98f433442e7cef46feaad28a49d3613a7d9e
Adon Metcalfe <adonm@fastmail.fm>
2020-09-05 07:04:07 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-if [ ! -d "glpi" ]; then
+set -e
+shopt -s dotglob
+
+# Download and extract glpi unless index.php exists
+# To overwrite/upgrade existing install rename index.php in your persistent dir
+if [ ! -f "index.php" ]; then
   curl -o /tmp/glpi.tgz -L https://github.com/glpi-project/glpi/releases/download/$GLPI_VERSION/glpi-$GLPI_VERSION.tgz
-  tar xvf /tmp/glpi.tgz
-  shopt -s dotglob
-  mv glpi/* .
+  tar xvf /tmp/glpi.tgz; rm /tmp/glpi.tgz
+  mv -v glpi/* .; rmdir glpi
 fi
-apache2-foreground
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+        set -- apache2-foreground "$@"
+fi
+exec "$@"
